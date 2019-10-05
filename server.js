10d1408a7e8ba4ccee6bec7f2d412a4106220a21
Tabatha Poonpurmsiri <tabatha.pp@gmail.com>
2019-10-05 18:16:06 -0400

    updated bunch

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
 var express = require("express");
-var bodyParser = require("body-parser");
 var session = require("express-session");
 var passport = require("./config/passport");
-
+var app = express()
 var PORT = process.env.PORT || 8080;
 
-var app = express()
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 app.use(express.static("public"));
 app.use(session({ secret: "keyboard cat" , resave: true, saveUninitialized}))
 app.use(passport.initialize())
