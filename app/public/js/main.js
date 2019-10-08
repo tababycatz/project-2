0b43187559af07bc46a8d753357d37707e2beaa0
Tabatha Poonpurmsiri <tabatha.pp@gmail.com>
2019-10-08 19:59:06 -0400

    updated all html pages

--- app/public/js/main.js
+++ app/public/js/main.js
@@ -2,4 +2,8 @@ $('.addcart').click(function(){
 
 var item = $(this).siblings('.item').text();
 var price = $(this).siblings('.price').text();
+var qty = $(this).siblings('.qty').text();
+var total = qty * price;
+
+
 });
